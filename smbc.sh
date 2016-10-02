ee1fad368587ce5f6352a23b52f0c1afdee4f3a8
ArenGamerZ <alex13gamerz@gmail.com>
2016-10-02 16:24:32 +0200

    Added documentation

--- smbc.sh
+++ smbc.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+##########################################################################################
+# Name: Smbcontrol                                                                 
+# Author: Aren <alex13gamerz@gmail.com>                                                   
+# Version: 2.0/stable                                                                           
+# Description: This script will make your administration of samba easier.
+#
+# LICENSE: GNU GPL    
+##########################################################################################
+
 if [[ $EUID -ne 0 ]];
 then
 	echo " "
@@ -8,27 +17,31 @@ then
 	exit 0
 
 else
+	# This enables the user shared
 	if [[ $1 = "-e" ]]; then
 		smbpasswd -e shared
 		service smbd restart
-		echo "Samba connections enabled successfully"
+		echo "Samba user shared enabled succesfully"
 		
-
+	# This disables the user shared
 	elif [[ $1 = "-d" ]]; then
 		smbpasswd -d shared
 		service smbd restart
-		echo "Samba connections disabled successfully"
+		echo "Samba user shared disabled successfully"
 
+	# This adds the IP from the second parameter "$2" and adds a rule to the firewall allowing the connection
 	elif [[ $1 = "-a" ]]; then
 		ufw limit in proto tcp from $2 to any port 135,139,445
 		ufw limit in proto udp from $2 to any port 137,138
 		echo "$2" >> /mnt/Data/IT/programming/myrepo/bash_scripts/smbcontrol/trusted_ips
 
+	# This removes the rule of the firewall added with the "-a" option below
 	elif [[ $1 = "-D" ]]; then
 		ufw delete limit in proto tcp from $2 to any port 135,139,445
 		ufw delete limit in proto udp from $2 to any port 137,138
 		sed -i "s|$2||;/^$/d" /mnt/Data/IT/programming/myrepo/bash_scripts/smbcontrol/trusted_ips
 
+	# This just shows a list of the trusted rules
 	elif [[ $1 = "-l" ]]; then
 		echo """The trusted networks are:
 		- $(cat /mnt/Data/IT/programming/myrepo/bash_scripts/smbcontrol/trusted_ips)"""
