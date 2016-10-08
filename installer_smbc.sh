1e85a243e42ba1f42e3923d83a5b301ad44f56fa
ArenGamerZ <alex13gamerz@gmail.com>
2016-10-08 12:34:26 +0200

    Minor changes

--- installer_smbc.sh
+++ installer_smbc.sh
@@ -7,9 +7,12 @@ then
 	echo "Error: This program must be run with root privileges"
 	echo ""
 else
-	ln -sf `pwd`/smbc.sh /bin/smbc
-	ln -sf `pwd`/smbc.sh /sbin/smbc
-	echo "Installation succeed !"
-	echo ""
+	mkdir -p /opt/smbcontrol
+	cp `pwd`/smbc.sh /opt/smbcontrol/smbc
+	chmod 755 /opt/smbcontrol/smbc
+	chmod 755 /opt/smbcontrol
+	ln -sf /opt/smbcontrol/smbc /bin/smbc
+	ln -sf /opt/smbcontrol/smbc /sbin/smbc
+	if [ -d /usr/share/smbcontrol ] && [ -e /opt/smbcontrol/smbc ]; then echo "Installation Succeed!"; else "Installation error!" && exit 1; fi
 fi
 exit 0
